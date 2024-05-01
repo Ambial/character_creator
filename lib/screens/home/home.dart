b78c649808ed2b72202927660a5b0b16ffe5e656
Simon Körner <simonkoerner@gmx.de>
2024-05-01 16:57:52 +0200

    Use ListView instead of individual elements

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,15 +1,18 @@
 import 'package:character_creator/shared/styled_button.dart';
 import 'package:character_creator/shared/styled_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _MyWidgetState();
+  State<Home> createState() => HomeState();
 }
 
-class _MyWidgetState extends State<Home> {
+class HomeState extends State<Home> {
+  List characters = ['Mario', 'Link', 'Aloy', 'Kratos', 'Nathan', 'Lara'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +23,17 @@ class _MyWidgetState extends State<Home> {
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
-              const StyledText('Character List'),
-              const StyledHeadline('Character List'),
-              const StyledTitle('Character List'),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: characters.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      tileColor: Colors.grey[800],
+                      title: StyledText(characters[index]),
+                    );
+                  },
+                ),
+              ),
               StyledButton(
                   onPressed: () {}, child: const StyledHeadline('Create new'))
             ],
