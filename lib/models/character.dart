631d2345e5b4f9a2aa94f20a8e4a37cd6a70a0f3
Simon Körner <simonkoerner@gmx.de>
2024-05-16 23:55:54 +0200

    Adding Getters for private properties

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,13 +1,16 @@
 import 'package:character_creator/models/stats.dart';
 
 class Character with Stats {
+  Character({required this.name, required this.slogan, required this.id});
+
   //fields
   final String name;
   final String slogan;
   final String id;
   bool _isFave = false;
 
-  Character({required this.name, required this.slogan, required this.id});
+  //getters
+  bool get isFave => _isFave;
 
   void toggleIsFave() {
     _isFave = !_isFave;
